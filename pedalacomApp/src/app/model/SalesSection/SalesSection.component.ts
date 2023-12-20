694ad8ed6441a73c68e78ad942efca96d1c5020b
MyNameIsAlpaca <saragosagabriele@gmail.com>
2023-12-20 17:41:25 +0100

    feat: home and contacts

--- pedalacomApp/src/app/model/SalesSection/SalesSection.component.ts
+++ pedalacomApp/src/app/model/SalesSection/SalesSection.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-saleSection',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './SalesSection.component.html',
   styleUrls: ['./SalesSection.component.scss']
 })
