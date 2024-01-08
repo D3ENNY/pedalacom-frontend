88f38e0fb39a6b35d7618048caf398f0bb7f995d
MyNameIsAlpaca <saragosagabriele@gmail.com>
2024-01-08 12:52:36 +0100

    feat: edit admin page

--- pedalacomApp/src/app/admin/models/delete-user/delete-user.component.ts
+++ pedalacomApp/src/app/admin/models/delete-user/delete-user.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CustomerApiServiceService } from '../../../shared/CRUD/customer-api-service.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-delete-user',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './delete-user.component.html',
   styleUrl: './delete-user.component.scss',
   providers :[CustomerApiServiceService]
@@ -13,15 +17,29 @@ export class DeleteUserComponent {
 
   constructor (private customer : CustomerApiServiceService) { }
 
+  userDelete = false;
+
+  userError = false;
+
+  userEmail:string =  '';
+
+  changeEmail(email: string){
+    this.userEmail = email;
+  }
+
   deleteUser( email : string) {
     console.log(email)
+    this.userError = false;
+    this.userDelete = false;
     this.customer.deleteCustomer(email).subscribe({
       next:(data: any) => {
         console.log("account eliminato");
         console.log(data);
+        this.userDelete = true;
       },
       error : (err : any) => {
         console.log(err)
+        this.userError = true;
       }
     })
   }
