c9214ceca6d408d82224d0d02f7f118c6e08101e
MyNameIsAlpaca <saragosagabriele@gmail.com>
2024-01-12 11:52:17 +0100

    Admin page control

--- pedalacomApp/src/app/admin/features/admin-panel/admin-panel.component.ts
+++ pedalacomApp/src/app/admin/features/admin-panel/admin-panel.component.ts
@@ -8,19 +8,31 @@ import { EditProductComponent } from '../../models/edit-product/edit-product.com
 import { DeleteUserComponent } from '../../models/delete-user/delete-user.component';
 import { ViewLogComponent } from '../../models/view-log/view-log.component';
 import { ViewErrorComponent } from '../../models/view-error/view-error.component';
+import { Router, RouterModule } from '@angular/router';
+
 
 @Component({
 	selector: 'app-admin-panel',
 	standalone: true,
 	imports: [AddProductsComponent, ManagementProductsComponent, RouterOutlet, RouterLink, CommonModule,EditProductComponent,DeleteUserComponent,
-	ViewLogComponent, ViewErrorComponent],
+	ViewLogComponent, ViewErrorComponent, RouterModule],
 	templateUrl: './admin-panel.component.html',
 	styleUrl: './admin-panel.component.scss',
 	providers: [PanelManagerService]
 })
 export class AdminPanelComponent {
 
-	constructor(private mainService: PanelManagerService) { }
+	constructor(private mainService: PanelManagerService, private router: Router) { }
+
+	ngOnInit(){
+		this.redirect()
+	}
+
+	redirect(){
+		if(localStorage.getItem("authorization") != btoa('Admin') && sessionStorage.getItem("authorization") != btoa('Admin')){
+		  this.router.navigate(['**']);
+		}
+	  }
 
 	stringPage = "";
 
