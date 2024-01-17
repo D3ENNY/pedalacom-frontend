372464a8fb10cf64ab99aaa6508069fb2f477ab4
MyNameIsAlpaca <saragosagabriele@gmail.com>
2024-01-17 12:34:45 +0100

    Pagina contacts

--- pedalacomApp/src/app/features/contacts/contacts.component.ts
+++ pedalacomApp/src/app/features/contacts/contacts.component.ts
@@ -10,4 +10,54 @@ import { CommonModule } from '@angular/common';
 })
 export class ContactsComponent {
 
+  valueOk: boolean = false;
+
+  messageSend: boolean = false;
+
+  errorList: string[] = []
+
+  sendMessage(){
+    this.messageSend = true;
+  }
+
+  checkValue(name: string, surname: string, email: string, object: string, message: string){
+    this.valueOk = false
+    this.errorList = []
+    if(!email.match("[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}")){
+      this.valueOk = true;
+      if(email != ""){
+        console.log(email)
+        this.errorList.push("Email non valida")
+      }
+    } 
+
+    if(!name.match("^[a-zA-Z ]{3,}$")){
+      this.valueOk = true;
+      if(name != ""){
+        this.errorList.push("Il nome non è valido")
+      }
+    }
+
+    if(!surname.match("^[a-zA-Z ]{3,}$")){
+      this.valueOk = true;
+      if(surname != ""){
+        this.errorList.push("Il cognome non è valido")
+      }
+    }
+
+    if(!object.match(".{3,}")){
+      this.valueOk = true;
+      if(object != ""){
+        this.errorList.push("L'oggetto del messaggio non è valido")
+      }
+    }
+
+    if(!message.match(".{3,}")){
+      this.valueOk = true;
+      if(message != ""){
+        this.errorList.push("Scrivi un messaggio valido")
+      }
+    }
+
+  }
 }
